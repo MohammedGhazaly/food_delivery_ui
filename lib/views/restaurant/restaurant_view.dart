a64c6be9b43f321fb352c1d10ec4f5263c7d8e1c
MohammedGhazaly <warendragon67@gmail.com>
2023-12-05 23:00:03 +0200

    Added menu grid view

--- lib/views/restaurant/restaurant_view.dart
+++ lib/views/restaurant/restaurant_view.dart
@@ -1,6 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/models/food.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
-import 'package:flutter_food_delivery_ui/shared_widgets/rating_stars.dart';
 import 'package:flutter_food_delivery_ui/views/restaurant/widgets/restaurant_details_section.dart';
 import 'package:flutter_food_delivery_ui/views/restaurant/widgets/restaurant_hero_section.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -12,19 +13,150 @@ class RestaurantView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          RestaurantHeroSection(
-            restaurant: restaurant,
-          ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            RestaurantHeroSection(
+              restaurant: restaurant,
+            ),
 
-          // Column()
-          SizedBox(
-            height: 12.h,
-          ),
-          RestaurantDetailsSection(restaurant: restaurant)
-        ],
+            // Column()
+            SizedBox(
+              height: 12.h,
+            ),
+            RestaurantDetailsSection(restaurant: restaurant),
+            SizedBox(
+              height: 12.h,
+            ),
+            Text(
+              "Menu",
+              style: TextStyle(
+                fontSize: 22.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 12.h,
+            ),
+            GridView.count(
+              crossAxisSpacing: 16.h,
+              mainAxisSpacing: 16.h,
+              padding: EdgeInsets.all(24.w),
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              children: List.generate(restaurant.menu.length, (index) {
+                return MenutItem(
+                  food: restaurant.menu[index],
+                );
+              }),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class MenutItem extends StatelessWidget {
+  final Food food;
+  const MenutItem({super.key, required this.food});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        // Image.asset(
+        //   food.imageUrl,
+        //   width: 175.h,
+        //   height: 175.h,
+        //   fit: BoxFit.cover,
+        // )
+        Container(
+          width: 175.h,
+          height: 175.h,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.r),
+            // gradient: LinearGradient(
+            //   colors: [
+            //     Colors.black,
+            //     Colors.black,
+            //   ],
+            // ),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                food.imageUrl,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          width: 175.h,
+          height: 175.h,
+          decoration: BoxDecoration(
+            // color: Colors.black.withOpacity(0.25),
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.black.withOpacity(0.35),
+                Colors.black87.withOpacity(0.35),
+                Colors.black54.withOpacity(0.35),
+                Colors.black38.withOpacity(0.35),
+              ],
+            ),
+            borderRadius: BorderRadius.circular(
+              16.r,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 175.h / 4,
+          child: Column(
+            children: [
+              AutoSizeText(
+                food.name,
+                minFontSize: 12,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "\$${food.price}",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+            bottom: 5,
+            right: 5,
+            child: Container(
+              width: 40.h,
+              height: 40.h,
+              decoration: BoxDecoration(
+                color: Colors.deepOrangeAccent,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: IconButton(
+                  padding: EdgeInsets.all(0),
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_rounded,
+                    size: 26.sp,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ))
+      ],
     );
   }
 }
