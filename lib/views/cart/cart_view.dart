f8b1f453b491e1e819621c660aa51d684fd0b338
MohammedGhazaly <warendragon67@gmail.com>
2023-12-06 22:59:51 +0200

    Finished foode deleviry app

--- lib/views/cart/cart_view.dart
+++ lib/views/cart/cart_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/user_data.dart';
 import 'package:flutter_food_delivery_ui/views/cart/widgets/cart_view_body.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
@@ -12,6 +13,31 @@ class CartView extends StatelessWidget {
         title: Text("Cart (${currentUser.cart.length})"),
       ),
       body: CartViewBody(),
+      bottomSheet: Container(
+        height: 75.h,
+        width: double.infinity,
+        decoration:
+            BoxDecoration(color: Theme.of(context).primaryColor, boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, -1),
+            blurRadius: 6.0,
+          ),
+        ]),
+        child: Center(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              "CHECKOUT",
+              style: TextStyle(
+                fontSize: 20.sp,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/views/cart/widgets/cart_view_body.dart
+++ lib/views/cart/widgets/cart_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/user_data.dart';
-import 'package:flutter_food_delivery_ui/models/order.dart';
 import 'package:flutter_food_delivery_ui/views/cart/widgets/cart_item.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,10 +9,68 @@ class CartViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double total = 0;
+
+    double totalCost = 0;
     return ListView.separated(
         itemBuilder: (context, index) {
-          return CartItem(
-            order: currentUser.cart[index],
+          if (index < currentUser.cart.length) {
+            totalCost += currentUser.cart[index].quantity *
+                currentUser.cart[index].food.price;
+            return CartItem(
+              order: currentUser.cart[index],
+            );
+          }
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Estimated delevity time",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "25 min",
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 8.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Total cost",
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      totalCost.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 75.h,
+                )
+              ],
+            ),
           );
         },
         separatorBuilder: (context, index) {
@@ -22,6 +79,6 @@ class CartViewBody extends StatelessWidget {
             color: Colors.grey,
           );
         },
-        itemCount: currentUser.cart.length);
+        itemCount: currentUser.cart.length + 1);
   }
 }
